52d2c9959911213db210aebc6c52cf40071f9ec6
”Anastasiia <nastja.gusarenko@gmail.com>
2021-07-20 20:02:18 +0300

    added hashrouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom'
+import {HashRouter, Redirect, Route, Switch} from 'react-router-dom'
 import {SuperComponents} from "../features/SuperComponents";
 import {PageNotFound} from "../features/404";
 import {Profile} from "../features/Profile";
@@ -12,22 +12,22 @@ import {MainPage} from "../features/MainPage";
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
     <div className={'App'}>
         <Switch>
             <Route exact path={'/'} render={() => <MainPage/>}/>
-            <Route exact path={'/newpassword'} render={() => <NewPassword />}/>
-            <Route exact path={'/passwordrecovery'} render={() => <PasswordRecovery />}/>
-            <Route exact path={'/profile'} render={() => <Profile />}/>
-            <Route exact path={'/signin'} render={() => <SignIn />}/>
-            <Route exact path={'/signup'} render={() => <SignUp />}/>
-            <Route exact path={'/supercomponents'} render={() => <SuperComponents />}/>
+            <Route path={'/newpassword'} render={() => <NewPassword />}/>
+            <Route path={'/passwordrecovery'} render={() => <PasswordRecovery />}/>
+            <Route path={'/profile'} render={() => <Profile />}/>
+            <Route path={'/signin'} render={() => <SignIn />}/>
+            <Route path={'/signup'} render={() => <SignUp />}/>
+            <Route path={'/supercomponents'} render={() => <SuperComponents />}/>
             <Route path={'/404'} render={() => <PageNotFound />}/>
             <Redirect from={'*'} to={'/404'}/>
         </Switch>
 
     </div>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
